1dcada89c485bd56b4fa2dd1a111081764e83d82
André Luiz <124028192+AndreLuizAlmeida@users.noreply.github.com>
2024-04-19 19:43:45 -0300

    Add files via upload

---
+++ Terceira atividade/CadastroVacinas/CadastroVacinas/Cidadao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CadastroVacinas
+{
+    class Cidadao
+    {
+        public string Cpf { get; }
+        public string Nome { get; }
+        public int Idade { get; }
+        public bool Vacinado { get; }
+        public string Telefone { get; }
+        public string Email { get; }
+        public DateTime? Agendamento { get; set; }
+
+        public Cidadao(string cpf, string nome, int idade, bool vacinado, string telefone, string email)
+        {
+            Cpf = cpf;
+            Nome = nome;
+            Idade = idade;
+            Vacinado = vacinado;
+            Telefone = telefone;
+            Email = email;
+        }
+
+        public void AgendarVacina(DateTime data)
+        {
+            Agendamento = data;
+        }
+    }
+}
---
+++ Terceira atividade/CadastroVacinas/CadastroVacinas/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CadastroVacinas
+{
+    class Funcionario
+    {
+        public string Nome { get; }
+        public string Matricula { get; }
+        public string CnpjPrestadora { get; }
+
+        public Funcionario(string nome, string matricula, string cnpjPrestadora)
+        {
+            Nome = nome;
+            Matricula = matricula;
+            CnpjPrestadora = cnpjPrestadora;
+        }
+    }
+}
